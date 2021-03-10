b43d72e4bcb1c52ad60476a11d57800c6bacb5fe
Jerry Park <haewon6640@gmail.com>
2021-03-10 20:18:34 +0900

    computing max profit problem

---
+++ buysellStockOnce.java
@@ -0,0 +1,15 @@
+/*
+* Buy and Sell a Stock Once
+* Design  an algorithm that determines the maximum profit 
+* that could have been made by buying and then selling a single share over a given day range, 
+* subject to the constraint htat the buy and the sell have to take place at the start of the day;
+* the sell must occur on a later day.
+* Ex) 
+* Input: [310,315,275,295,260,270,290,230,255,250]
+* Output: 30 (290-260)
+*/
+public class buysellStockOnce {
+    public static void main(String[] args) {
+        
+    }
+}
