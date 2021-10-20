fa1fea27cb2ff3b3e68cb7a63c899a9da5876a0d
Jerry Park <haewon6640@gmail.com>
2021-10-20 12:38:49 +0900

    Linked List Cycle II

--- Leetcode/60Leet.java
+++ Leetcode/60Leet.java
@@ -22,3 +22,24 @@ public class Solution {
       return false;
   }
 }
+
+/**
+* Linked list Cycle II
+**/
+public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null && slow != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode cycleHead = head;
+                while (cycleHead != slow) {
+                    cycleHead = cycleHead.next;
+                    slow = slow.next;
+                }
+                return cycleHead;
+            }
+        }
+        return null;
+    }
