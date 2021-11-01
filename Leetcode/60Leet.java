1889b2fd72ffb76aad9ecbd933b006d2e7fdaf9a
Jerry Park <haewon6640@gmail.com>
2021-11-01 14:52:42 +0900

    Update 60Leet.java

--- Leetcode/60Leet.java
+++ Leetcode/60Leet.java
@@ -82,5 +82,14 @@ public boolean isSameTree(TreeNode p, TreeNode q) {
 /*
 * Invert Binary Tree
 */
+public TreeNode invertTree(TreeNode root) {
+    if (root == null) {
+        return null;
+    }
+    final TreeNode left = root.left, right = root.right;
+    root.left = invertTree(right);
+    root.right = invertTree(left);
+    return root;
+}
 
 
