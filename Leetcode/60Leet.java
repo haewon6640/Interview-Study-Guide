cf87df2616cddb43439898a01412acbd7250aedb
Jerry Park <haewon6640@gmail.com>
2021-11-10 10:05:49 +0900

    Update 60Leet.java

--- Leetcode/60Leet.java
+++ Leetcode/60Leet.java
@@ -152,3 +152,26 @@ public class Codec {
     }
   }
 }
+
+/*
+* Subtree of Another Tree
+*/
+
+public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    if (root == null) {
+        return false;
+    }
+    return sameTree(root,subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right,subRoot);
+}
+public boolean sameTree(TreeNode root, TreeNode subRoot) {
+    if (root == null && subRoot == null) {
+        return true;
+    }
+    if (root == null && subRoot != null) {
+        return false;
+    }
+    if (root != null && subRoot == null) {
+        return false;
+    }
+    return root.val == subRoot.val && sameTree(root.left, subRoot.left) && sameTree(root.right,subRoot.right);
+}
