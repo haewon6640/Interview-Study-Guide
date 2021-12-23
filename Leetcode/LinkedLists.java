317cf73339d28f43cfb69d486da9f92f19f987f7
Jerry Park <haewon6640@gmail.com>
2021-12-23 18:49:57 +0900

    Linked Lists
    Cut Tree and add it to new file

---
+++ Leetcode/LinkedLists.java
@@ -0,0 +1,52 @@
+/*
+* 
+*
+* https://leetcode.com/discuss/general-discussion/460599/blind-75-leetcode-questions
+*/
+
+/*
+*
+* Linked Lists
+*
+*/
+
+/*
+* Linked list Cycle
+*/
+
+public class Solution {
+  public boolean hasCycle(ListNode head) {
+      ListNode fast = head;
+      ListNode slow = head;
+      while (fast != null && fast.next != null && slow != null) {
+          fast = fast.next.next;
+          slow = slow.next;
+          if (fast == slow) {
+              return true;
+          }
+      }
+      return false;
+  }
+}
+
+/**
+* Linked list Cycle II
+**/
+public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null && slow != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow) {
+                ListNode cycleHead = head;
+                while (cycleHead != slow) {
+                    cycleHead = cycleHead.next;
+                    slow = slow.next;
+                }
+                return cycleHead;
+            }
+        }
+        return null;
+    }
+}
