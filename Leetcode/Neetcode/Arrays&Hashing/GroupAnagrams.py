a599ec9d811799e2ddd5571c2e99bf932f9b7e36
Haewon6640 <haewon6640@gmail.com>
2022-08-29 14:38:27 -0400

    groupAnagrams

--- Leetcode/Neetcode/Arrays&Hashing/GroupAnagrams.py
+++ Leetcode/Neetcode/Arrays&Hashing/GroupAnagrams.py
@@ -1,3 +1,12 @@
 # https://leetcode.com/problems/group-anagrams/
 
-# def groupAnagrams(strs):
+def groupAnagrams(strs):
+    map1 = {}
+    for str in strs:
+        word = tuple(sorted(str))
+        if word not in map1:
+            map1[word] = []
+        map1[word].append(str)
+    return list(map1.values())
+
+print(groupAnagrams( ["eat","tea","tan","ate","nat","bat"]))
